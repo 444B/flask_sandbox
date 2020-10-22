64084c5cd310c25c26dfc08ca2ef2b7680b6ead1
blue42 <444b+blue42@protonmail.ch>
2020-10-22 12:44:25 +0300

    extends base + url_for

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 
-
+Questions:
+-vim
+-MVC arch
 
 
 
 
 TO ACTIVATE:
 source virtual/bin/activate
-	-deactivate
+To DEACTIVATE VENV:
+deactivate
 
 
 TO RUN:
 - export FLASK_APP=app.py
 - export FLASK_ENV=development
 - flask run
+
+Topics covered:
+- block content
+- jinja
+- url_for
--- app.py
+++ app.py
@@ -3,11 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def indef():
+def index():
 	title = '444B\'s portfolio'
 	return render_template('index.html', title=title)
 
 @app.route('/about')
 def about():
+	title = 'About page'
+	return render_template('about.html', title=title)
+
+@app.route('/sandbox')
+def sandbox():
+	title = 'Sandbox'
 	names = ['john', 'mary', 'wess']
-	return render_template('about.html', names=names)
+	return render_template('sandbox.html', title=title, names=names)
