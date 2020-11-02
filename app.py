799ff4544be8e4593b3793c9d175df38fd6c945e
Blue42 <444b@protonmail.ch>
2020-11-02 22:47:02 +0200

    Search-V, Entry-V, index-UC

--- app.py
+++ app.py
@@ -1,49 +1,72 @@
 from flask import Flask, render_template, request
 import sqlite3
-# import os
+import sys
 
 
-# currentdirectory = os.path.dirname(os.path.abspath(__file__))
 
 # app config
 app = Flask(__name__)
 
 # Flask Site Structure
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
 	title = '444B\'s portfolio'
+	try:
+		if request.method == 'GET':
+			search_term = request.args.get('search_term')
+			connection = sqlite3.connect('products.db')
+			cursor = connection.cursor()
+			cursor.execute("SELECT Price from product_table WHERE Product = ? ", (search_term,))
+			result = cursor.fetchone()[0]
+			return render_template ("index.html", result=result)
+		if request.method == 'POST':
+			product = request.form.get('product')
+			price = request.form.get('price')
+			connection = sqlite3.connect('products.db')
+			cursor = connection.cursor()
+			cursor.execute('INSERT INTO product_table VALUES (NULL,?,?)', (product, price))
+			connection.commit()
+			return render_template('index.html', product=product, price=price)
+	except Exception as e:
+		search_term = request.args.get('search_term')
+		print(e, file=sys.stderr)
+		print("{n} does not exist".format(n=search_term))
+		return render_template ("index.html")
+
 	return render_template('index.html', title=title)
 
 @app.route('/product_entry', methods=['GET', 'POST'])
 def entry():
 	title = 'Product Entry Page'
 	if request.method == 'GET':
-		name = request.form['name']
-		connection = sqlite3.connect('products.db')
-		cursor = connection.cursor()
-		query1 = "INSERT INTO product_table VALUES('{n}', {p})".format(n = name, p = price)
-		cursor.execute(query1)
-		connection.commit()
 		return render_template('product_entry.html')
 	if request.method == 'POST':
-		return render_template('product_entry.html')
+		product = request.form.get('product')
+		price = request.form.get('price')
+		connection = sqlite3.connect('products.db')
+		cursor = connection.cursor()
+		cursor.execute('INSERT INTO product_table VALUES (NULL,?,?)', (product, price))
+		connection.commit()
+		return render_template('product_entry.html', product=product, price=price)
 
 @app.route('/product_search', methods=['GET', 'POST'])
 def search():
 	title = 'Product Search'
 	try:
 		if request.method == 'GET':
-			name = request.args.get('Product')
+			name = request.args.get('name')
 			connection = sqlite3.connect('products.db')
 			cursor = connection.cursor()
-			query1 = "SELECT Price from product_table WHERE Product = '{n}'".format(n = name)
-			result = cursor.execute(query1)
-			result = result.fetchall()[0][0]
-			return render_template ("product_search.html", Price = Result)
+			cursor.execute("SELECT Price from product_table WHERE Product = ? ", (name,))
+			result = cursor.fetchone()[0]
+			return render_template ("product_search.html", result=result)
 		if request.method == 'POST':
-			return render_template ("product_search.html", Price = '')
-	except:
-			return render_template ("product_search.html", Price = '')
+			return render_template ("product_search.html")
+	except Exception as e:
+		name = request.args.get('name')
+		print(e, file=sys.stderr)
+		print("{n} does not exist".format(n=name))
+		return render_template ("product_search.html")
 
 
 @app.route('/subscribe')
